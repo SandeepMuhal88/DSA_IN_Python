1b1bac0893d4b4fe7ae49e2c90d765c1a6b7ff6d
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-26 17:41:21 +0530

    Big O notation Problem

--- BIg_O_Notation/Problem-06.py
+++ BIg_O_Notation/Problem-06.py
@@ -1,1 +1,15 @@
 # Q6. store every element of list in another list using for loop and find time and space complexity ?
+def anotherList(l1):
+    newList = []
+    for i in l1:
+        newList.append(i)
+    print(newList)
+
+
+l1 = [10, 20, 30]
+anotherList(l1)
+
+
+
+# time complexity = o(n)
+# space complexity = 0(n)
