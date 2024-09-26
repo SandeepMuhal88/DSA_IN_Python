b8ae4c1852be6cb8a3915da37b5583357e9bddce
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-26 17:42:26 +0530

    Big O notation Problems

--- BIg_O_Notation/Problem-07.py
+++ BIg_O_Notation/Problem-07.py
@@ -1,1 +1,13 @@
 # Q7. find sum of element of list and also find time and space complexity ?
+def findSum(l1):
+    add = 0
+    for i in l1:
+        add += i
+    print(f"sum of list : {add}")
+
+
+findSum([1, 2, 3, 4, 5, 6])
+
+
+# time complexity = o(n)
+# space complexity =  o(n)
