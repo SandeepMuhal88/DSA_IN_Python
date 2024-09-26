d0103a7d049dacd11f0b63bc175cd3eeaf39bad1
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-26 17:38:06 +0530

    Solve Problem-05

--- BIg_O_Notation/Problem-05.py
+++ BIg_O_Notation/Problem-05.py
@@ -1,1 +1,14 @@
 # Q5. find unique pair in dice ?
+for i in range(1,7):
+    for j in range(i,7):
+        print(i,j)
+
+
+# unique()
+#  time complexity = o(1)
+def Unique():
+    for i in range(1,7):
+       for j in range(i,7):
+           print(i,j)
+
+Unique()
